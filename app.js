3c6a3a663f69239b2a5bc152bee6a504db67f1ad
IH8U <grzesiek.duda06@gmail.com>
2023-12-06 02:27:42 +0100

    estetyczne zmiany, dodanie ikonki nazwy i levela

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let summonerName;
 
 const searchSummoner = () => {
   summonerName = document.getElementById("summoner-name").value;
-  const summonerLevel = data();
+  data();
 };
 
 const data = async () => {
@@ -17,5 +17,26 @@ const data = async () => {
   console.log(dataSummoner);
 
   const summonerLevel = dataSummoner.summonerLevel;
+  const summonerIcon = dataSummoner.profileIconId;
   console.log(summonerLevel);
+  console.log(summonerIcon);
+
+  // USTAWIANIE NA STRONIE
+  let info = document.getElementById("summoner-info");
+  info.style.display = "flex";
+  let iconUrl =
+    "url('https://ddragon.leagueoflegends.com/cdn/13.24.1/img/profileicon/" +
+    summonerIcon +
+    ".png')";
+  let icon = document.getElementById("icon");
+  icon.style.background = iconUrl;
+  icon.style.backgroundSize = "contain";
+  icon.style.backgroundPosition = "center";
+
+  let name = document.getElementById("name");
+  name.innerText = summonerName;
+
+  let lvl = document.getElementById("lvl");
+  lvl.innerText = summonerLevel;
+  // !!!!!!!!!
 };
