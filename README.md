3bcff9910c068920747924a10229907aa56f0b27
IH8U <grzesiek.duda06@gmail.com>
2023-12-12 00:26:13 +0100

    summoner stats

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 ## Używa [API Riot Games](https://developer.riotgames.com/apis) i [CommunityDragon](https://raw.communitydragon.org/)
 
+### otwórz [index.html](index.html)
+
 ### Zwraca:
 
 - Poziom
 - Ikonę
+- Statystyki gracza:
+  - ranga na solo queue i flex
+  - liczba wygranych i przegranych
 - listę 10 ostatnich gier:
   - champion
   - poziom
@@ -18,8 +23,9 @@
 - Bartoxiq
 - imSfn
 - CzarnownikZQuaja
-- MemeIH8U
 - DuDi
-- i może jakiś uczeń inny, który gra w LoLa może się pochwali nickiem to się sprawdzi
+- i może jakiś uczeń inny, który gra w LoLa może się pochwali nickiem to się sprawdzi (tylko EUNE)
 
 ###### jeśli coś nie zadziała to troche dziwne bo u mnie działa albo trzeba wygenerować nowy api key
+
+![main page img](https://media.discordapp.net/attachments/1008097952599900182/1183894326355427419/image.png?ex=6589fe83&is=65778983&hm=c54ce00fb7ca37a5a92dbf0b4aada72c52f701658af15f518dfb22a17e103adf&=&format=webp&quality=lossless&width=1201&height=675)
--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const APIKey = "RGAPI-d6f6427e-170b-4038-b4f6-e93a2fe905ba";
+// TODO:
+// POZAMIENIAC WSZYSTKIE ZMIENNE GDZIE DODAJE COS DO STRINGA NA TE TAKIE TE NO `COS TU ${ZMIENNA} COS TU`
+
+const APIKey = "RGAPI-1e3a1099-fb22-4933-85fe-066d2a0e6f17";
 const euneUrl = "https://eun1.api.riotgames.com";
 const europeUrl = "https://europe.api.riotgames.com";
 const championUrl =
@@ -7,8 +10,8 @@ let summonerName;
 let champions = [];
 const championIconUrl =
   "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/";
-
-// ---------------------------------------------
+let leagueUrl =
+  "https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/";
 
 const getChampions = async () => {
   let dataChampions = await fetch(
@@ -24,15 +27,11 @@ const getChampions = async () => {
 };
 getChampions();
 
-// ---------------------------------------------
-
 const searchSummoner = () => {
   summonerName = document.getElementById("summoner-name").value;
   document.getElementById("last-games").innerHTML = "";
   data();
 };
-
-// ---------------------------------------------
 
 const data = async () => {
   // LVL NAZWA I IKONKA
@@ -63,7 +62,65 @@ const data = async () => {
 
   let lvl = document.getElementById("lvl");
   lvl.innerText = summonerLevel;
-  // ---------------------------------------------
+
+  // STATYSTYKI GRACZA
+  leagueUrl += dataSummoner.id + "?api_key=" + APIKey;
+  let summonerStats = await fetch(leagueUrl);
+  summonerStats = await summonerStats.json();
+  const summonerStatsDOM = document.getElementById("statistics");
+  summonerStatsDOM.style.background = "#ffffff11";
+  let color;
+
+  for (const element of summonerStats) {
+    let statsDOM = document.createElement("div");
+    switch (element.tier) {
+      case "IRON":
+        color = "#614F4E";
+        break;
+      case "BRONZE":
+        color = "#7F5F57";
+        break;
+      case "SILVER":
+        color = "#C5CBD5";
+        break;
+      case "GOLD":
+        color = "#DAB079";
+        break;
+      case "PLATINUM":
+        color = "#288FC9";
+        break;
+      case "EMERALD":
+        color = "#43a981";
+        break;
+      case "DIAMOND":
+        color = "#7c79c0";
+        break;
+      case "MASTER":
+        color = "#BA5BE9";
+        break;
+      case "GRANDMASTER":
+        color = "#F07235";
+        break;
+      case "CHALLENGER":
+        color = "#3596e3";
+        break;
+      default:
+        color = "#fff";
+        break;
+    }
+    statsDOM.innerHTML = `${
+      element.queueType
+    } <span style = "color: ${color};">${element.tier} ${element.rank} ${
+      element.leaguePoints
+    }LP </span><br><span style = "color: green;">Wins: ${
+      element.wins
+    }</span><br><span style = "color: red;">Losses: ${
+      element.losses
+    }</span><br>winratio: ${
+      Math.round((element.wins / (element.losses + element.wins)) * 100) / 100
+    }%`;
+    summonerStatsDOM.appendChild(statsDOM);
+  }
 
   // OSTATNIE GRY
   let puuid = dataSummoner.puuid;
@@ -71,7 +128,7 @@ const data = async () => {
     europeUrl +
     "/lol/match/v5/matches/by-puuid/" +
     puuid +
-    "/ids?start=0&count=20&api_key=" +
+    "/ids?start=0&count=10&api_key=" +
     APIKey;
 
   let lastGamesIDs = await fetch(matchesUrl);
@@ -99,9 +156,7 @@ const data = async () => {
         let kills = participant.kills;
         let deaths = participant.deaths;
         let assists = participant.assists;
-        // let lastGamesDOM = document.getElementById("last-games");
-        // let isWinDOM = document.createElement("h2");
-        // let gameDOM = document.createElement("div");
+
         if (win) {
           gameDOM.classList = "win";
           isWinDOM.innerHTML = "WIN";
@@ -110,7 +165,6 @@ const data = async () => {
           isWinDOM.innerHTML = "LOOSE";
         }
 
-        // let championIconDOM = document.createElement("div");
         for (const champion of champions) {
           if ((champion.id = championID)) {
             championIconDOM.style.background =
@@ -121,11 +175,9 @@ const data = async () => {
           }
         }
 
-        // let championLevelDOM = document.createElement("div");
         championLevelDOM.innerText = champLevel;
         championLevelDOM.classList = "champ-level";
 
-        // let kdaDOM = document.createElement("div");
         kdaDOM.innerText = kills + " / " + deaths + " / " + assists;
         kdaDOM.classList = "KDA";
 
@@ -137,5 +189,5 @@ const data = async () => {
       }
     }
   }
-  console.log(dataSummoner.id);
+  // console.log(dataSummoner.id);
 };
